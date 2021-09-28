b634583b6fe67b049c75fd4876d7b2d6368a015d
Mute <40270424+Ced0@users.noreply.github.com>
2021-09-28 14:46:15 +0200

    Health Bar
    Prefab of an health bar

---
+++ Assets/Script/HealthBarController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour
+{
+    public Slider slider;
+
+    // Start is called before the first frame update
+    public void SetMaxHp(int maxHp)
+    {
+        slider.maxValue = maxHp;
+        slider.value = maxHp;
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+}
